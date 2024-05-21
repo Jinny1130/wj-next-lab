0568056ceff57df0cbaf1ef9fc883394d485954a
choiwoojin <ggogo33@paymint.co.kr>
2024-05-21 10:14:52 +0900

    [MODIFY] header image 경로 수정

--- src/components/common/Header.tsx
+++ src/components/common/Header.tsx
@@ -5,7 +5,7 @@ const Header = () => {
     return (
         <div id="header" className="header w-full h-14 flex items-center justify-between px-3 fixed bg-white z-10">
             <Link href={'/'} className="h-full py-2.5 flex items-center font-bold">
-                <Image src="/wj.png" alt="woojin_imoji" width={30} height={30} priority /> Woojin | Front-end Dev.
+                <Image src="/images/wj.png" alt="woojin_imoji" width={30} height={30} priority /> Woojin | Front-end Dev.
             </Link>
 
             <div className=""></div>
