5be65286919470c9b6e51d2916519a21d074d174
choiwoojin <ggogo33@paymint.co.kr>
2024-05-24 12:55:09 +0900

    [MODIFY] 번호판 노출시 dimed & iphone top dimed 추가, textarea 추가

--- src/components/send-message/NumberButtonBox.tsx
+++ src/components/send-message/NumberButtonBox.tsx
@@ -19,7 +19,9 @@ const NumberButtonBox: React.FC<BoxProps> = (boxProps) => {
     const isFirstSettingNumbers = (selectedNumbers.join('') === '12345678');
     
     useEffect(() => {
+
         setNumberTextColor(isFirstSettingNumbers);
+
     }, [selectedNumbers])
 
     // 전화번호 text color
@@ -94,7 +96,10 @@ const NumberButtonBox: React.FC<BoxProps> = (boxProps) => {
     const [ showNumberBoard, setShowNumberBoard ] = useState<boolean>(false);
 
     useEffect(() => {
+
         showAndHideNumberBoard();
+        setMobileTopAreaTheme();
+
     }, [showNumberBoard])
 
     const arrowIconInfo = {
@@ -103,40 +108,64 @@ const NumberButtonBox: React.FC<BoxProps> = (boxProps) => {
         iconFill: '#FFF'
     }
 
-    function clickArrowButton(e:any) {
-        setShowNumberBoard(!showNumberBoard)
-    }
-
+    // mobile - 숫자패드 보이기
     function showAndHideNumberBoard() {
         document.querySelector('.show-hide-button')?.nextElementSibling?.classList.remove( showNumberBoard ? 'h-[40px]' : 'h-[600px]' );
         document.querySelector('.show-hide-button')?.nextElementSibling?.classList.add( showNumberBoard ? 'h-[600px]' : 'h-[40px]' );
     }
+    
+    // mobile - top 영역 dimed 과 동일한 스타일컬러 지정 (eg. safari 로 진행시 iphone top area)
+    function setMobileTopAreaTheme() {
+        if (showNumberBoard) {
+            const metaTag = document.createElement('meta');
+
+            metaTag.setAttribute('name', 'theme-color');
+            metaTag.setAttribute('content', '#145dbe');
+            metaTag.id = 'safari-top-area';
+
+            document.head.appendChild(metaTag);
+        }
+        else {
+            const themeMetaTag = document.querySelector('#safari-top-area') 
+
+            if (themeMetaTag !== null && document.head.contains(themeMetaTag)) {
+                document.head.removeChild(themeMetaTag);
+            }
+        }
+    }
 
     return (
-        <div className={`flex flex-col`}>
-            <button className="show-hide-button flex item-center justify-center px-[10px] pt-[8px] pb-[18px] text-base font-bold text-white" onClick={ () => setShowNumberBoard(!showNumberBoard) }>
-                { <ArrowIcon iconInfo={ arrowIconInfo } className={ `${showNumberBoard ? 'rotate-180' : 'rotate-0'} mr-1` } /> }{`${showNumberBoard ? 'CLOSE' : 'OPEN'} THE NUMBER PAD`}
-            </button>
+        <div className={`relative size-full p-6 sm:p-0 sm:flex sm:flex-col sm:justify-between ${showNumberBoard ? 'before:block before:fixed before:size-full before:inset-x-0 before:inset-y-0 before:bg-black before:opacity-25' : ''}`}>
+            
+            <div className={`send-text-area w-full flex flex-col justify-between sm:mb-[80px]`}>
+                <div className={`w-full h-1/3 flex items-center justify-center text-center ${FontJua.className} text-[32px]`}>📮</div>
+                <textarea placeholder="보내시려는 메시지를 입력해주세요 " className={`resize-none w-full h-2/3 min-h-[300px] p-[20px] [background:var(--wt-100,#FFF)] shadow-[0_-8px_30px_rgba(0,0,0,0.25)] sm:shadow-[0px_10px_20px_0px_rgba(0,0,0,0.10),0px_36px_72px_0px_rgba(0,0,0,0.10)] rounded-[20px] overflow-hidden border-collapse placeholder:text-gray-400 placeholder:text-[13px]`} />
+            </div>
 
-            <div className={`w-full h-[40px] sm:h-fit transition-all sm:transition-none ease-linear delay-50 sm:delay-0 [background:var(--wt-100,#FFF)] shadow-[0_-8px_30px_rgba(0,0,0,0.25)] sm:shadow-[0px_10px_20px_0px_rgba(0,0,0,0.10),0px_36px_72px_0px_rgba(0,0,0,0.10)] rounded-t-[48px] sm:rounded-[48px] overflow-hidden ${boxProps.className}`}>
-                <div className="[background:var(--GY-20,#FAFAFA)] flex flex-col items-start gap-2 flex-[1_0_0] p-12">
-                    <p className={`text-[color:var(--GY-900,#2B2B2B)] text-xl font-bold leading-6 mb-2 ${FontJua.className}`}>전화번호를 입력해 주세요.</p>
-                    <div className={`flex items-center text-[40px] font-bold leading-[normal] ${FontJua.className}`}>
-                        <p className="start-phone-number mr-[10px]" style={{ fontSize: '40px' }}>010</p>
-                        {
-                            selectedNumbers.map((number, index) =>
-                                <p id={`number_${index + 1}`} key={index} className={`send-target-numbers ${index === 3 ? 'mr-[10px]' : ''} text-[color:var(--GY-100,#E5E5E5)]`}>{number}</p>
-                            )
-                        }
+            <div className={`w-full absolute sm:relative bottom-0 left-0 z-10`}>
+                <button className="show-hide-button w-full flex sm:hidden item-center justify-center px-[10px] pt-[8px] pb-[18px] text-base font-bold text-white" onClick={ () => setShowNumberBoard(!showNumberBoard) }>
+                    { <ArrowIcon iconInfo={ arrowIconInfo } className={ `${showNumberBoard ? 'rotate-180' : 'rotate-0'} mr-1` } /> }{`${showNumberBoard ? 'CLOSE' : 'OPEN'} THE NUMBER PAD`}
+                </button>
+                <div className={`w-full h-[40px] sm:h-fit transition-all sm:transition-none ease-linear delay-50 sm:delay-0 [background:var(--wt-100,#FFF)] shadow-[0_-8px_30px_rgba(0,0,0,0.25)] sm:shadow-[0px_10px_20px_0px_rgba(0,0,0,0.10),0px_36px_72px_0px_rgba(0,0,0,0.10)] rounded-t-[48px] sm:rounded-[48px] overflow-hidden ${boxProps.className}`}>
+                    <div className="[background:var(--GY-20,#FAFAFA)] flex flex-col items-start gap-2 flex-[1_0_0] p-12">
+                        <p className={`text-[color:var(--GY-900,#2B2B2B)] text-xl font-bold leading-6 mb-2 ${FontJua.className}`}>전화번호를 입력해 주세요.</p>
+                        <div className={`flex items-center text-[40px] font-bold leading-[normal] ${FontJua.className}`}>
+                            <p className="start-phone-number mr-[10px]" style={{ fontSize: '40px' }}>010</p>
+                            {
+                                selectedNumbers.map((number, index) =>
+                                    <p id={`number_${index + 1}`} key={index} className={`send-target-numbers ${index === 3 ? 'mr-[10px]' : ''} text-[color:var(--GY-100,#E5E5E5)]`}>{number}</p>
+                                )
+                            }
+                        </div>
                     </div>
-                </div>
-                <div className="flex items-center flex-col sm:items-start sm:flex-row sm:min-h-[400px]">
-                    <div className="w-full sm:w-1/2 min-w-[400px] sm:min-w-[50%] px-[48px] pt-8 sm:pt-[56px] pb-4 sm:pb-[48px]">
-                        <h3 className="text-[color:var(--GY-700,#555)] text-[15px] sm:text-xl font-medium leading-7 mb-2 sm:mb-4">입력하신 전화번호로 <br className="hidden sm:block" />체험 메시지가 발송됩니다.</h3>
-                        <p className=" text-[color:var(--GY-500,#808080)] text-xs font-normal leading-[18px]">입력된 휴대전화번호는 메시지 발송 이외에 <br className="hidden sm:block" />다른 목적으로 사용하거나 수집하지 않습니다.</p>
-                    </div>
-                    <div className={`buttons-wrap w-full sm:w-1/2 min-w-[400px] sm:min-w-[50%] px-[30px] sm:px-[70px] pt-6 sm:pt-[40px] pb-[30px] sm:pb-[72px] flex flex-wrap justify-between text-xl ${FontJua.className}`}>
-                        { NumberButtonsRender() }
+                    <div className="flex items-center flex-col sm:items-start sm:flex-row sm:min-h-[400px]">
+                        <div className="w-full sm:w-1/2 min-w-[400px] sm:min-w-[50%] px-[48px] pt-8 sm:pt-[56px] pb-4 sm:pb-[48px]">
+                            <h3 className="text-[color:var(--GY-700,#555)] text-[15px] sm:text-xl font-medium leading-7 mb-2 sm:mb-4">입력하신 전화번호로 <br className="hidden sm:block" />체험 메시지가 발송됩니다.</h3>
+                            <p className=" text-[color:var(--GY-500,#808080)] text-xs font-normal leading-[18px]">입력된 휴대전화번호는 메시지 발송 이외에 <br className="hidden sm:block" />다른 목적으로 사용하거나 수집하지 않습니다.</p>
+                        </div>
+                        <div className={`buttons-wrap w-full sm:w-1/2 min-w-[400px] sm:min-w-[50%] px-[30px] sm:px-[70px] pt-6 sm:pt-[40px] pb-[30px] sm:pb-[72px] flex flex-wrap justify-between text-xl ${FontJua.className}`}>
+                            { NumberButtonsRender() }
+                        </div>
                     </div>
                 </div>
             </div>
