b8dfdb54844a79f1fadee934fa11aa5bf53457e8
choiwoojin <ggogo33@daum.net>
2024-05-26 15:01:25 +0900

    [MODIFY] next.config.mjs AWS key 수정

--- next.config.mjs
+++ next.config.mjs
@@ -6,9 +6,9 @@ dotenv.config();
 const nextConfig = {
     reactStrictMode: false,
     env: {
-        AWS_ACCESS_KEY_ID: process.env.NEXT_PUBLIC_AWS_ACCESS_KEY_ID,
-        AWS_SECRET_ACCESS_KEY: process.env.NEXT_PUBLIC_AWS_SECRET_ACCESS_KEY,
-        AWS_REGION: process.env.NEXT_PUBLIC_AWS_REGION,
+        NEXT_PUBLIC_AWS_ACCESS_KEY_ID: process.env.NEXT_PUBLIC_AWS_ACCESS_KEY_ID,
+        NEXT_PUBLIC_AWS_SECRET_ACCESS_KEY: process.env.NEXT_PUBLIC_AWS_SECRET_ACCESS_KEY,
+        NEXT_PUBLIC_AWS_REGION: process.env.NEXT_PUBLIC_AWS_REGION,
     },
 };
 
