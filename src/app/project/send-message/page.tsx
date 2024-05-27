2a0decb8a2fb03f08898b89250763b99b923a931
choiwoojin <ggogo33@paymint.co.kr>
2024-05-27 13:37:30 +0900

    [MODIFY] yarn.lock 제거 & 메시지발송 페이지 탭 타이틀 변경

--- src/app/project/send-message/page.tsx
+++ src/app/project/send-message/page.tsx
@@ -5,7 +5,7 @@ import NumberButtonBox from '@/components/send-message/NumberButtonBox'
 export default function SendMessageMain() {
     return (
         <>
-            <title>메시지 발송 Demo | WOOJINNY</title>
+            <title>Send SMS ✉️ | WOOJINNY</title>
 
             <div className="fixed inset-x-0 inset-y-0 size-full [background:var(--BL-600,#1A7CFD)]">
                 <div className="size-full sm:p-[72px] flex flex-col justify-end items-center self-stretch">
