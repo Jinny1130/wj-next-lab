05a8d40e20968c221c0d5873afc132cd0faab16d
choiwoojin <ggogo33@daum.net>
2024-06-02 02:50:08 +0900

    [MODIFY] footer 간격 수정

--- src/components/common/Footer.tsx
+++ src/components/common/Footer.tsx
@@ -1,11 +1,11 @@
-const Footer = ({ isMobile }:Readonly<{isMobile: string}>) => {
+const Footer = () => {
     return (
-        <div className="max-w-4xl mx-auto px-6 text-center flex flex-col items-center justify-center py-24 text-gray-400 text-[13px]">
+        <div className={`max-w-4xl mx-auto px-6 text-center flex flex-col items-center justify-center py-24 text-gray-400 text-[13px] bg-transparent`}>
             <p>
                 본 페이지는 상업적 목적이 아닌 
-                {isMobile === 'mo' ? <br/> : <></>}
+                <br className="block sm:hidden"/>
                 비영리적 개인 포트폴리오용으로 만들어진 사이트입니다.
-                <br/><br/>
+                <br/>
                 본 페이지는 React, Next.js, Vercel 을 이용해서 제작하였습니다.
                 <br/>
             </p>
