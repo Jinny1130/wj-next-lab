097680c393a86fa7403591ed4db52deec4c1a9a7
choiwoojin <ggogo33@daum.net>
2024-06-02 02:53:59 +0900

    [MODIFY] 불필요 props 제거

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -56,7 +56,7 @@ export default function Home() {
                 <SideProject />
             </div>
 
-            <Footer isMobile={isMobile} />
+            <Footer />
         </div>
     );
 }
