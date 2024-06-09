3d01213053fb33c7aa6acc1c26eecd2112043bbb
choiwoojin <ggogo33@daum.net>
2024-06-09 14:45:44 +0900

    [MODIFY] resume contact&skills 영역 map 으로 코드 수정

--- src/components/resume/ContactAndSkill.tsx
+++ src/components/resume/ContactAndSkill.tsx
@@ -1,6 +1,17 @@
 import Image from "next/image"
 import Title from './Title';
 
+const contact = {
+    Email: 'jinnyc1130@gmail.com',
+    Github: 'https://github.com/jinny1130',
+}
+
+const haveSkills = [
+    { title: 'Strong', skillsName: [ 'Vue', 'Vuex', 'Javascript', 'Html', 'CSS', 'SCSS' ] },
+    { title: 'Knowledge', skillsName: [ 'React', 'Next.js', 'Typescript', 'Vercel', 'Webpack', 'node.js ' ] },
+    { title : 'Used Tool', skillsName: [ 'Git', 'Github', 'GitLab', 'VSCode', 'WebStorm', 'Figma', 'Slack' ] },
+]
+
 const ContactAndSkill = () => {
     return (
         <div className="contact-wrap w-full flex flex-col items-center sm:flex-row sm:justify-between">
@@ -10,63 +21,45 @@ const ContactAndSkill = () => {
 
             <div className="w-full sm:w-2/3">
                 
-                <Title title="Contact & Channel" size="S" />
+                <Title title="Contact & Channel" size="M" />
                 <div className="px-1 py-2">
-                    <div className="flex items-center">
-                        <p className="w-1/4 text-sm flex font-semibold text-gray-800 items-baseline">
-                            <i className="block w-1 h-1 rounded-full bg-sky-400 mr-1"/>Phone
-                        </p>
-                        <a className="w-3/4 text-sm text-gray-400 underline hover:text-sky-400" href="sms:050-6602-1642">050-6602-1642</a>
-                    </div>
-                    <div className="flex items-center">
-                        <p className="w-1/4 text-sm flex font-semibold text-gray-800 items-baseline">
-                            <i className="block w-1 h-1 rounded-full bg-sky-400 mr-1"/>Email
-                        </p>
-                        <a className="w-3/4 text-sm text-gray-400 underline hover:text-sky-400" href="mailto:cwjin94@gmail.com">jinnyc1130@gmail.com</a>
-                    </div>
-                    <div className="flex items-center">
-                        <p className="w-1/4 text-sm flex font-semibold text-gray-800 items-baseline">
-                            <i className="block w-1 h-1 rounded-full bg-sky-400 mr-1"/>Github
-                        </p>
-                        <a className="w-3/4 text-sm text-gray-400 underline hover:text-sky-400" href="https://github.com/jinny1130" target="_blank">https://github.com/jinny1130</a>
-                    </div>
+                    {
+                        Object.entries(contact).map(([key, value]) => {
+                            return (
+                                <div className="flex items-center" key={key}>
+                                    <p className="w-1/4 text-sm flex font-semibold text-gray-800 items-baseline">
+                                        <i className="block w-1 h-1 rounded-full bg-sky-400 mr-1"/>{key}
+                                    </p>
+                                    <a className="w-3/4 text-sm text-gray-400 underline hover:text-sky-400" href={`${key === 'Email' ? 'mailto:' : ''}${value}`} target={key === 'Github' ? '_blank' : ''}>
+                                        {value}
+                                    </a>
+                                </div>
+                            )
+                        })
+                    }
                 </div>
 
-                <div className="flex items-start pt-4">
-                    <div className="w-1/2">
-                        <Title title="Tech Stack" size="S" />
-                        <div className="w-full text-sm px-1 py-2">
-                            <p className="flex items-baseline">
-                                <i className="block w-1 h-1 rounded-full bg-sky-400 mr-1"/>Vue, Vuex
-                            </p>
-                            <p className="flex items-baseline">
-                                <i className="block w-1 h-1 rounded-full bg-sky-400 mr-1"/>Javascript
-                            </p>
-                            <p className="flex items-baseline">
-                                <i className="block w-1 h-1 rounded-full bg-sky-400 mr-1"/>Html, CSS, SCSS
-                            </p>
-                            <p className="flex items-baseline">
-                                <i className="block w-1 h-1 rounded-full bg-sky-400 mr-1"/>React, Next.js
-                            </p>
-                            <p className="flex items-baseline">
-                                <i className="block w-1 h-1 rounded-full bg-sky-400 mr-1"/>Typescript
-                            </p>
-                        </div>
-                    </div>
-
-                    <div className="w-1/2">
-                        <Title title="Used Tool" size="S" />
-                        <div className="w-full text-sm px-1 py-2">
-                            <p className="flex items-baseline">
-                                <i className="block w-1 h-1 rounded-full bg-sky-400 mr-1"/>Git, Github, GitLab
-                            </p>
-                            <p className="flex items-baseline">
-                                <i className="block w-1 h-1 rounded-full bg-sky-400 mr-1"/>VSCode, WebStorm
-                            </p>
-                            <p className="flex items-baseline">
-                                <i className="block w-1 h-1 rounded-full bg-sky-400 mr-1"/>Figma, Slack, Monday
-                            </p>
-                        </div>
+                <div className="sm:flex sm:flext-start pt-5">
+                    <Title title="Skills" size="M"/>
+                    <div className="mt-3 sm:ml-8 sm:mt-0">
+                        {
+                            haveSkills.map((skill, index) => {
+                                return (
+                                    <div className="w-full pb-2" key={index}>
+                                        <Title title={skill.title} size="S" />
+                                        <div className="flex flex-wrap w-full text-sm px-1 py-2">
+                                        { skill.skillsName.map((name, idx) => {
+                                            return (
+                                                <p className="flex items-baseline mr-3 mb-2" key={idx}>
+                                                    <i className="block w-1 h-1 rounded-full bg-sky-400 mr-1"/>{name}
+                                                </p>
+                                            )
+                                        }) }
+                                        </div>
+                                    </div>
+                                )
+                            })
+                        }
                     </div>
                 </div>
             </div>
