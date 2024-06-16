358c3e07a5fd34d253792a94ad482b57852d949f
choiwoojin <ggogo33@daum.net>
2024-06-16 20:24:09 +0900

    [MODIFY] resume 이름, introduce 영역 수정

--- src/components/resume/NameWrap.tsx
+++ src/components/resume/NameWrap.tsx
@@ -4,7 +4,7 @@ const NameWrap = () => {
             <i className="block w-2 h-12 bg-sky-200 mr-1.5" />
             <div className="flex items-end">
                 <p className="font-semibold text-4xl mr-1.5">최우진</p>
-                <p className="text-xl mr-1.5">Woojin Choi</p>
+                <p className="text-xl mr-1.5">Front-end Dev.</p>
             </div>
         </div>
     )
