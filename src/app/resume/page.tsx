85b285fd34b69de3737db419fc0b59400e0b2413
choiwoojin <ggogo33@daum.net>
2025-01-07 21:26:49 +0900

    update : introduce update

--- src/app/resume/page.tsx
+++ src/app/resume/page.tsx
@@ -5,16 +5,12 @@ import Header from '@/components/common/Header';
 import NameWrap from "@/components/resume/NameWrap";
 import Contact from '@/components/resume/Contact'
 import Introduce from "@/components/resume/Introduce";
-import Career from "@/components/resume/Career";
 import WorkExperience from "@/components/resume/WorkExperience";
-import WorkExperienceOld from "@/components/resume/WorkExperience_old";
 import OtherExperience from "@/components/resume/OtherExperience";
 import Footer from '@/components/common/Footer';
 import { useEffect, useState } from "react"
 
 export default function ResumeMain () {
-    const [isMobile, setIsMobile] = useState('pc');
-
     useEffect( () => {
 
         function setHeaderShadow() {
@@ -26,20 +22,11 @@ export default function ResumeMain () {
             }
         }
 
-        function handleResize() {
-            setIsMobile(window.innerWidth < 640 ? 'mo' : 'pc'); // 640px 미만은 모바일로 간주
-        }
-
-        // 초기 width 사이즈 확인
-        handleResize();
-
         window.addEventListener('scroll', setHeaderShadow);
-        window.addEventListener('resize', handleResize);
 
         // 언마운트 시 리스너 제거
         return () => {
             window.removeEventListener('scroll', setHeaderShadow);
-            window.removeEventListener('resize', handleResize);
         }
 
     }, [])
@@ -54,10 +41,8 @@ export default function ResumeMain () {
                 <div className="resume-wrap max-w-4xl pt-24 px-6 mx-auto sm:px-3">
                     <NameWrap />
                     <Contact />
-                    <Introduce isMobile={isMobile} />
-                    {/* <Career isMobile={isMobile} /> */}
+                    <Introduce />
                     <WorkExperience />
-                    <WorkExperienceOld />
                     <OtherExperience />
                 </div>
 
--- src/components/resume/Introduce.tsx
+++ src/components/resume/Introduce.tsx
@@ -1,6 +1,6 @@
 import Title from "./Title"
 
-const Introduce = ({ isMobile }:Readonly<{isMobile: string}>) => {
+const Introduce = () => {
     return (
         <div>
             <Title title="Introduce" size="L"/>
@@ -8,10 +8,10 @@ const Introduce = ({ isMobile }:Readonly<{isMobile: string}>) => {
                 <p>
                     안녕하세요, <b>프론트엔드 개발자</b> 최우진 입니다.
                     <br/>
-                    3년차 웹 프론트 개발자로 재직중이며, 다양한 사람과의 소통을 좋아합니다.
+                    <b>3년차</b> 프론트엔드로 재직중이며, 다양한 사람과의 소통을 좋아합니다.
                     <br/><br/>
                     솔직하고 담백한 화법을 선호하며, 동료들과의 논의를 통해 하나의 목표를 이뤄 나가는 과정을 즐깁니다. 또한, 팀 개발자분들과 함께하는 리뷰 시간을 중요하게 생각합니다.
-                    저는 이러한 다양한 소통의 과정에서 얻는 배움을 통해 성장해 나가고 있습니다.
+                    저는 이러한 다양한 <b>소통의 과정에서 얻는 배움</b>을 통해 성장해 나가고 있습니다.
                     <br/><br/>
                     아래의 내용은 업무 과정에서 경험한 <b>Work Experience</b>, 개인 프로젝트를 개발하고 기술을 학습하며 경험한 <b>Other Experience</b>, 그리고 그 과정에서 익혀온 <b>Skills</b>로 구성되어 있습니다.
                 </p>
