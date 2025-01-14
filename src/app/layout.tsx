9c0ea3b23c8b0ab494babc8e4c347ada5623792f
choiwoojin <ggogo33@paymint.co.kr>
2025-01-14 17:52:05 +0900

    design : design modify

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,8 +5,8 @@ import "@/styles/globals.css";
 const NanumGothic = Nanum_Gothic({ weight: ['400', '700', '800'], subsets: ['latin'] });
 
 export const metadata: Metadata = {
-	title: "최우진 | Front-end",
-	description: "안녕하세요👋🏻 프론트엔드 개발자 최우진입니다.",
+	// title: "최우진 | Front-end",
+	// description: "안녕하세요👋🏻 프론트엔드 개발자 최우진입니다.",
 	icons: {
 		icon: '/images/wj.png'
 	}
